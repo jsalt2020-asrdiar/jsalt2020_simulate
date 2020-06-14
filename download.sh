28ef3bde1181bbb47932c26209298d64fc1170b7
Takuya Yoshioka <takuya.yoshioka@hotmail.co.jp>
2020-06-14 15:58:33 -0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -7,23 +7,23 @@ DATADIR=$EXPROOT/data-orig
 
 
 
-if [ -d $DATADIR ]
-then
-    echo "Downloading LibriSpeech training data."    
-    CWD=`pwd`
-    mkdir -p $DATADIR
+CWD=`pwd`
+mkdir -p $DATADIR
 
-    cd $DATADIR
+cd $DATADIR
 
-    # wget
-    wget --no-check-certificate http://www.openslr.org/resources/12/train-clean-100.tar.gz
-    wget --no-check-certificate http://www.openslr.org/resources/12/train-clean-360.tar.gz
+# wget
+[ ! -f ./train-clean-100.tar.gz ] && wget --no-check-certificate http://www.openslr.org/resources/12/train-clean-100.tar.gz
+[ ! -f ./train-clean-360.tar.gz ] && wget --no-check-certificate http://www.openslr.org/resources/12/train-clean-360.tar.gz
+[ ! -f ./dev-clean.tar.gz ] && wget --no-check-certificate http://www.openslr.org/resources/12/dev-clean.tar.gz
+[ ! -f ./test-clean.tar.gz ] && wget --no-check-certificate http://www.openslr.org/resources/12/test-clean.tar.gz
 
-    # unpack    
-    tar -zxvf train-clean-100.tar.gz
-    tar -zxvf train-clean-360.tar.gz
+# unpack    
+[ ! -d ./LibriSpeech/train-clean-100 ] && tar -zxvf train-clean-100.tar.gz
+[ ! -d ./LibriSpeech/train-clean-360 ] && tar -zxvf train-clean-360.tar.gz
+[ ! -d ./LibriSpeech/dev-clean ] && tar -zxvf dev-clean.tar.gz
+[ ! -d ./LibriSpeech/test-clean ] && tar -zxvf test-clean.tar.gz
 
-    cd $CWD
-fi
+cd $CWD
 
 
